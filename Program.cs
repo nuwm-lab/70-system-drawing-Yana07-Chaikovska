59dbae22a0157562355cbd98863a063ee12094b4
Yana07-Chaikovska <chaikovska_ak24@nuwm.edu.ua>
2025-11-23 15:45:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Windows.Forms;
 
-namespace GraphProject
-{
     internal static class Program
     {
         [STAThread]
